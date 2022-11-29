763cdbee5435881bf181fd288db4631d01e4312f
Saraesp <saraesposito145@gmail.com>
2022-11-29 14:39:31 +0100

    divisione problema in parti

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+//1 - Scrivi in console i numeri da 1 a 100
+//1.1 - Stampa per i multipli di 3 "Fizz"
+//1.2 - Stampa pe ri multipli di 5 "Buzz"
+//1.3 - Stampa per i multipli sia di 3 che di 5 "FizzBuzz"
+//1.4 - Per i numeri che non sono multipli ne di 3 ne di 5 non stampare nulla
+
