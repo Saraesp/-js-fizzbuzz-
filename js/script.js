1d9b3a9b9fac8ff0513a6df1c54f8ba5a4735ee4
Saraesp <saraesposito145@gmail.com>
2022-11-30 15:24:54 +0100

    impostazione style.css e correzione js

--- js/script.js
+++ js/script.js
@@ -6,17 +6,27 @@
 //4 - Stampa per i multipli sia di 3 che di 5 "FizzBuzz"
 //5 - Per i numeri che non sono multipli ne di 3 ne di 5 non stampare il numero
 
+const container = document.querySelector(".container")
+
 for(let i=1; i<=100; i++){
     if(i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBuzz")
+        let word = 'FizzBuzz';
+        container.innerHTML += `<div class="square square-fizzbuzz">${word}</div>`;
+        console.log(word);
     }
     else if(i % 5 == 0){
-        console.log("Buzz")
+        let word = 'Buzz';
+        container.innerHTML += `<div class="square square-buzz">${word}</div>`;
+        console.log(word);
     }
     else if(i % 3 == 0){
-        console.log("Fizz")
+        let word = 'Fizz';
+        container.innerHTML += `<div class="square square-fizz">${word}</div>`;
+        console.log(word);
     }
     else{
-        console.log("i")
+        console.log(i)
+        container.innerHTML += `<div class="square square-common">${i}</div>`;
     }
 }
+
